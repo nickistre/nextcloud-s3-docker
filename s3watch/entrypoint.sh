3275fb8a9a7b5473b2f0487d28b55d3d9222f2ed
Nicholas Istre <nickistre@gmail.com>
2020-05-24 14:04:37 -0700

    Fixed up docker files to work better with converting to Kubernetes

--- s3watch/entrypoint.sh
+++ s3watch/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-mc config host add s3 http://s3:9000 $S3_ACCESS_KEY $S3_SECRET_KEY
+mc config host add s3 http://$S3_HOST:$S3_PORT $S3_ACCESS_KEY $S3_SECRET_KEY
 mc mb -p s3/$S3_BUCKETNAME
 mc watch s3/$S3_BUCKETNAME
